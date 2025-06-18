0e4181492b375124f224405754c1c882f26c62bc
Atharva Bhosale <atharvagoldi@gmail.com>
2025-06-18 23:02:43 +0530

    optimised for phones and tablets, added css styling for form

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -6,18 +6,20 @@ const MainComponent = () => {
         <form >
             <div className="form-group first-child">
                 <label htmlFor="target-muscle">Target Muscle: </label>
-                <input type="text" id="target-muscle" />
+                <input type="text" id="target-muscle" className="input-fields" />
             </div>
             <br />
              <div className="form-group">
                 <label htmlFor="weight">Last Used Weight: </label>
-                <input type="number" id="weight" />
+                <input type="number" id="weight" className="input-fields" />
             </div>
             <br />
              <div className="form-group">
                 <label htmlFor="reps">Last Reps: </label>
-                <input type="text" id="reps" />
+                <input type="text" id="reps" className="input-fields" />
             </div>
+
+            <button className="submit-btn">Find Overload!</button>
         </form>
     </div>
   )
